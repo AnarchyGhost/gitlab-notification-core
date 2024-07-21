9559c4059e532f697cbe60dbf017deb3ab7073c8
AnarchyGhost <k.markelov@anarchyghost.com>
2024-07-21 11:33:29 +0300

    0.0.5: Added usersById, usersByUsername, labelsByName map

--- src/main/kotlin/com/anarchyghost/models/domain/GitlabEvent.kt
+++ src/main/kotlin/com/anarchyghost/models/domain/GitlabEvent.kt
@@ -5,7 +5,7 @@ import com.anarchyghost.models.domain.event.types.BaseEvent
 import com.anarchyghost.models.json.configuration.labels.LabelsConfigurationJson
 import com.anarchyghost.models.json.configuration.users.UsersConfigurationJson
 
-data class GitlabEvent<T: BaseEvent>(
+class GitlabEvent<T: BaseEvent>(
     val event: T,
     val projectId: String?,
     val groupId: String?,
@@ -13,14 +13,16 @@ data class GitlabEvent<T: BaseEvent>(
     val addedLabels: Set<String>,
     val deletedLabels: Set<String>,
     val type: EventType,
-    private val usersMapping: List<UsersConfigurationJson>,
-    private val labelsMapping: List<LabelsConfigurationJson>,
+    val usersMapping: List<UsersConfigurationJson>,
+    val labelsMapping: List<LabelsConfigurationJson>,
 ) {
+    val usersById = usersMapping.associateBy { it.gitlab.id.toString() }
+    val usersByUsername = usersMapping.associateBy { it.gitlab.username }
+    val labelsByName = labelsMapping.associateBy { it.gitlab.name }
 
-    //TODO think about nullsafety
-    fun getUserById(id: Long): UsersConfigurationJson = usersMapping.first { it.gitlab.id == id }
-    fun getUserByUsername(username: String): UsersConfigurationJson =
-        usersMapping.first { it.gitlab.username == username }
-    fun getLabelByName(name: String): LabelsConfigurationJson = labelsMapping.first { it.gitlab.name == name }
+    fun getUserById(id: Long): UsersConfigurationJson? = usersById[id.toString()]
+    fun getUserByUsername(username: String): UsersConfigurationJson? =
+        usersByUsername[username]
+    fun getLabelByName(name: String): LabelsConfigurationJson? = labelsByName[name]
     fun getLabelsByName(labels: Set<String>): List<LabelsConfigurationJson> = labelsMapping.filter { it.gitlab.name in labels }
 }
